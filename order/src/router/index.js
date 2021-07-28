d9b6c3ca05d8dc852db898ec7051405cc348f3a3
atsun1 <515087727@qq.com>
2021-07-28 17:51:39 +0800

    no message

--- order/src/router/index.js
+++ order/src/router/index.js
@@ -11,29 +11,29 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld
     },
-	{
-	  path: '/about',
-	  component: ()=>import('@/views/about.vue')
-	},
-	{
-	  path: '/home',
-	  component: ()=>import('@/views/home.vue')
-	},
-	{
-	  path: '/login',
-	  component: ()=>import('@/views/login.vue')
-	},
-	{
-	  path: '/register',
-	  component: ()=>import('@/views/register.vue')
-	},
-	{
-	  path: '/menu',
-	  component: ()=>import('@/views/menu.vue')
-	},
-	{
-	  path: '/admin',
-	  component: ()=>import('@/views/admin.vue')
-	}
+    {
+      path: '/about',
+      component: ()=>import('@/views/about.vue')
+    },
+    {
+      path: '/home',
+      component: ()=>import('@/views/home.vue')
+    },
+    {
+      path: '/login',
+      component: ()=>import('@/views/login.vue')
+    },
+    {
+      path: '/register',
+      component: ()=>import('@/views/register.vue')
+    },
+    {
+      path: '/menu',
+      component: ()=>import('@/views/menu.vue')
+    },
+    {
+      path: '/admin',
+      component: ()=>import('@/views/admin.vue')
+    }
   ]
 })
